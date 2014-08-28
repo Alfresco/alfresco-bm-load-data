c1daa7bf00da3c84dd1134eee4fea64a2f6baa30
Derek Hulley <derek.hulley@alfresco.com>
2014-08-28 20:39:59 +0000

    BM Dataload 2.0: Use newer property inheritance format
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/benchmark/tests/dataload/trunk@82223 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- src/test/java/org/alfresco/bm/BMDataLoadTest.java
+++ src/test/java/org/alfresco/bm/BMDataLoadTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2012 Alfresco Software Limited.
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -18,10 +18,28 @@
  */
 package org.alfresco.bm;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Properties;
+
+import org.alfresco.bm.data.DataCreationState;
+import org.alfresco.bm.site.SiteDataService;
+import org.alfresco.bm.site.SiteDataServiceImpl;
 import org.alfresco.bm.tools.BMTestRunner;
+import org.alfresco.bm.tools.BMTestRunnerListenerAdaptor;
+import org.alfresco.bm.user.UserData;
+import org.alfresco.bm.user.UserDataService;
+import org.alfresco.bm.user.UserDataServiceImpl;
+import org.alfresco.mongo.MongoDBForTestsFactory;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.springframework.context.ApplicationContext;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClientURI;
 
 /**
  * Initiates the application context associated with this test.
@@ -31,12 +49,82 @@ import org.junit.runners.JUnit4;
  * @since 2.0
  */
 @RunWith(JUnit4.class)
-public class BMDataLoadTest
+public class BMDataLoadTest extends BMTestRunnerListenerAdaptor
 {
-    @Test
-    public void testPublicApi() throws Exception
+    private MongoDBForTestsFactory mongoDBForTestsFactory;
+    private DB db;
+    private String uriWithoutDB;
+    private String mongoTestHost;
+    private String usersCollection;
+    private String sitesCollection;
+    private String siteMembersCollection;
+    private UserDataService userDataService;
+    private SiteDataService siteDataService;
+    
+    @Before
+    public void setUpDB() throws Exception
     {
+        mongoDBForTestsFactory = new MongoDBForTestsFactory();
+        db = mongoDBForTestsFactory.getObject();
+        uriWithoutDB = mongoDBForTestsFactory.getMongoURIWithoutDB();
+        mongoTestHost = new MongoClientURI(uriWithoutDB).getHosts().get(0);
+        
+        usersCollection = "mirror.test.users";
+        sitesCollection = "mirror.test.sites";
+        siteMembersCollection = "mirror.test.siteMembers";
+        
+        userDataService = new UserDataServiceImpl(db, usersCollection);
+        siteDataService = new SiteDataServiceImpl(db, sitesCollection, siteMembersCollection);
+        // Create some users
+        for (int i = 0; i < 10; i++)
+        {
+            String domain = String.format("D%2d", i);
+            for (int j = 0; j < 100; j++)
+            {
+                String username = String.format("U%3d", j);
+                UserData user = new UserData();
+                user.setCreationState(DataCreationState.Created);
+                user.setDomain(domain);
+                user.setEmail(username + "@" + domain);
+                user.setFirstName("U");
+                user.setLastName(String.format("%3d", j));
+                user.setPassword(username);
+                user.setUsername(username);
+                userDataService.createNewUser(user);
+            }
+        }
+    }
+    
+    @After
+    public void tearDownDB() throws Exception
+    {
+        mongoDBForTestsFactory.destroy();
+    }
+    
+    @Test
+    public void testBasic() throws Exception
+    {
+        Properties props = new Properties();
+        props.setProperty("mirror.users", usersCollection);
+        props.setProperty("mirror.sites", sitesCollection);
+        props.setProperty("mirror.siteMembers", siteMembersCollection);
+        
         BMTestRunner testRunner = new BMTestRunner(60000L);
-        testRunner.run(null, null, null);
+        testRunner.addListener(this);
+        testRunner.run(null, mongoTestHost, props);
+    }
+
+    @Override
+    public void testRunStarted(ApplicationContext testCtx, String test, String run)
+    {
+        assertEquals(1000L, userDataService.countUsers(null, DataCreationState.Created));
+    }
+
+    @Override
+    public void testRunFinished(ApplicationContext testCtx, String test, String run)
+    {
+        // Expect 10 sites per domain
+        assertEquals(100L, siteDataService.countSites(null, null));
+        assertEquals(10L, siteDataService.countSites("D01", DataCreationState.Failed));
     }
 }
